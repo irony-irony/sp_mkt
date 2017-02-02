dda061753a9c6b7ba8ba37a4d64c4f1260a928f0
irony-irony <tomoya.isemura@gmail.com>
2017-02-02 16:58:41 +0900

    migrationやり直し

--- db/migrate/20170131055604_add_agreement_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddAgreementToUser < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :agreement, :boolean
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170131055604) do
+ActiveRecord::Schema.define(version: 20170201120951) do
 
   create_table "spaces", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -33,12 +33,17 @@ ActiveRecord::Schema.define(version: 20170131055604) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "family_name"
-    t.string   "first_name"
+    t.string   "given_name"
     t.string   "avator"
     t.boolean  "agreement"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
