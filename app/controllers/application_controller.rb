f7993f036bf8c2a8a8f38c8b3e0f6b8794b3adf8
irony-irony <tomoya.isemura@gmail.com>
2017-02-03 19:24:34 +0900

    追加パラメーターの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_parmitted_paramaters, if: :devise_controller?
+
+  def configure_parmitted_paramaters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:family_name, :given_name, :avator, :agreement])
+  end
+
+  def after_sign_out_path_for(resource) #サインアウトした後のリダイレクト先の指定
+    'spaces_path'
+  end
+
+  def after_sign_up_path_for(resource) #サインアップした後のリダイレクト先の指定
+    'new_user_confirmation_path'
+  end
 end
